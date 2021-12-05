6224d8d5b386842e1bf6af31f30ace6251fee62f
Sébastien Vincent <metabast@gmail.com>
2021-12-05 16:13:26 +0100

    add resize singleton; add 2 shaders and 2 plane

--- src/worlds/world/World.js
+++ src/worlds/world/World.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
 import Content from './Content.js';
+import Resize from '../../commons/Resize.js'
 	
 export default function World(){
 
@@ -10,23 +11,28 @@ export default function World(){
 		
 		const renderer = new THREE.WebGLRenderer({
 			canvas: canvas,
-			antialias: false,
+			antialias: true,
 		});
 		renderer.setSize( window.innerWidth, window.innerHeight );
 
 		const control = new OrbitControls(camera, renderer.domElement)
 		control.enableDamping = true;
-		camera.position.set( 0, 0, 1 );
+		camera.position.set( 0, 0, 2 );
 
 		const clock = new THREE.Clock();
 
 		// 
 		const content = new Content(scene);
 
+		Resize.getInstance()
+			.updateCanvas(canvas)
+			.updateCamera(camera)
+			.updateRenderer(renderer);	
+
 
 		function animate(){
 			control.update();
-			content.update();
+			content.update(clock);
 			renderer.render(scene, camera);
 			requestAnimationFrame(animate);
 		}
